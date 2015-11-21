d697452f0190b61a137039aee389ad84c4d46f57
Samu <samuelareas@hotmail.com>
2015-11-21 02:53:39 +0100

    Verifica Datos

--- app/src/main/java/com/clubesuvigo/sunshine/LoginActivity.java
+++ app/src/main/java/com/clubesuvigo/sunshine/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final int DATOS_OK = 0;
+    public String error_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,6 @@ public class LoginActivity extends AppCompatActivity {
         final EditText contraseña_editText = (EditText) findViewById(R.id.contraseña_login);
         final EditText repite_contraseña = (EditText) findViewById(R.id.repetir_contraseña_login);
         Button enviar_button = (Button) findViewById(R.id.button_enviar);
-
         enviar_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,15 +28,48 @@ public class LoginActivity extends AppCompatActivity {
 
                 int result = validarDatos(nombre_string,contraseña_string,repitecontraseña_string);
 
-                if(result==DATOS_OK){
+                if(result==1){
                     Toast.makeText(LoginActivity.this, nombre_string, Toast.LENGTH_SHORT).show();
+                }else{
+
+                    Toast.makeText(LoginActivity.this, getString(error_login),Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private int validarDatos(String nombre_string, String contraseña_string, String repitecontraseña_string) {
+    private String getString(String error_login) {
+        return error_login;
+    }
 
+    public int validarDatos(String nombre_string, String contraseña_string, String repitecontraseña_string) {
+        int DATOS_OK = 0;
+        if(nombre_string.isEmpty() == false) {
+            if (nombre_string.length() > 3) {
+                if (contraseña_string.isEmpty() == false) {
+                    if (contraseña_string.length() > 6) {
+                        if (contraseña_string.contentEquals(repitecontraseña_string)) {
+                            DATOS_OK = 1;
+                        } else {
+                            error_login = "Las contraseñas no son iguales.";
+                            DATOS_OK = 0;
+                        }
+                    } else {
+                        error_login = "La contraseña debe contener al menos 7 caracteres.";
+                        DATOS_OK = 0;
+                    }
+                } else {
+                    error_login = "Debes escribir una contraseña.";
+                    DATOS_OK = 0;
+                }
+            } else {
+                error_login = "El nombre de usuario debe contener al menos 4 caracteres.";
+                DATOS_OK = 0;
+            }
+        }else{
+            error_login = "Debes escribir un nombre de usuario";
+            DATOS_OK = 0;
+        }
         return DATOS_OK;
     }
 
